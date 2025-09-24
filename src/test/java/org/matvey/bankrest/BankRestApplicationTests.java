058e6e1c916c96e0f5dec027aa5199d41c060713
Matvey <m5221211m@gmail.com>
2025-09-24 13:46:17 +0300

    init: initial commit

---
+++ src/test/java/org/matvey/bankrest/BankRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.matvey.bankrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
