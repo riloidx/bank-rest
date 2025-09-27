dc23b085bbefcfb5076d36a316b2313cf54e39a8
Matvey <m5221211m@gmail.com>
2025-09-27 20:13:03 +0300

    refactor: delete useless imports

--- src/main/java/org/matvey/bankrest/controller/AuthController.java
+++ src/main/java/org/matvey/bankrest/controller/AuthController.java
@@ -4,13 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.matvey.bankrest.dto.request.LoginDto;
 import org.matvey.bankrest.dto.request.RegistrationDto;
 import org.matvey.bankrest.dto.response.AuthResponseDto;
-import org.matvey.bankrest.dto.response.UserResponseDto;
-import org.matvey.bankrest.entity.User;
-import org.matvey.bankrest.mapper.UserMapper;
-import org.matvey.bankrest.security.CustomUserDetails;
-import org.matvey.bankrest.security.JwtUtil;
 import org.matvey.bankrest.service.AuthService;
-import org.matvey.bankrest.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-    private final JwtUtil jwtUtil;
-    private final UserMapper userMapper;
 
     @PostMapping("/registration")
     public ResponseEntity<AuthResponseDto> registration(@RequestBody RegistrationDto registrationDto) {
